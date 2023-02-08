260199b4209bfc23311e3e6a0518dbd02bfaf0a5
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-02-08 20:19:03 -0300

    37º dia de CTD - Projeto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 40 * i);
+      setTimeout(() => elemento.innerHTML += letra, 30 * i);
     });
   }
 
